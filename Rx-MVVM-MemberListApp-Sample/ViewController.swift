c0e5c4194b9fb97ed4d684c53292f66c8f11501d
katafuchix <katafuchix@gmail.com>
2026-02-04 11:17:50 +0900

    コード修正

--- Rx-MVVM-MemberListApp-Sample/ViewController.swift
+++ Rx-MVVM-MemberListApp-Sample/ViewController.swift
@@ -48,8 +48,8 @@ class ViewController: UIViewController {
         // セル選択
         self.tableView.rx.modelSelected(Member.self)
             .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [unowned self] member in
-                let vc = R.storyboard.main.memberDetailViewController()!
+            .subscribe(onNext: { [weak self] member in
+                guard let self = self, let vc = R.storyboard.main.memberDetailViewController() else { return }
                 vc.member.accept(member)
                 self.present(vc, animated: true)
             })
@@ -58,24 +58,27 @@ class ViewController: UIViewController {
         // メンバー取得中はMBProgressHUDを表示
         self.viewModel.outputs
             .isLoading.asDriver(onErrorJustReturn: false)
+            .distinctUntilChanged()
             .drive(MBProgressHUD.rx.isAnimating(view: self.view))
             .disposed(by: rx.disposeBag)
         
+        /*
         // activityIndicator
         self.viewModel.outputs
             .members
             .observe(on: MainScheduler.instance)
             .map { $0.isEmpty }
+            .distinctUntilChanged() // true/false が切り替わった時だけアニメーションを呼ぶ
             .subscribe(onNext: { [weak self] visible in
                 self?.setVisibleWithAnimation(self?.activityIndicator, visible)
             })
             .disposed(by: rx.disposeBag)
+        */
         
         // 再読み込み
-        self.loadButton.rx.tap.asDriver()
-            .drive(onNext: { [unowned self] _ in
-                self.viewModel.loadData()
-            })
+        // loadData()メソッドを介さず、triggerに直接流し込む
+        self.loadButton.rx.tap
+            .bind(to: self.viewModel.inputs.trigger)
             .disposed(by: rx.disposeBag)
     }
     
